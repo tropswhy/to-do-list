1d1dd2d2421327efdd22079c11dfc3507301e383
tropswhy <solodkovnikita08@gmail.com>
2024-01-19 21:13:35 +0300

    init

--- src/main.js
+++ src/main.js
@@ -1,5 +1,21 @@
 import { createApp } from 'vue'
 import './style.css'
+import { createPinia } from 'pinia'
+import { createRouter, createWebHistory } from 'vue-router'
+import Main from './Main.vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const routes = [
+    {
+        path: '/',
+        component: Main,
+    },
+
+]
+const pinia = createPinia()
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+})
+
+createApp(App).use(router).use(pinia).mount('#app')
---
+++ src/stores/myStore.js
@@ -0,0 +1,9 @@
+import { defineStore } from 'pinia'
+
+export const useMyStore = defineStore('myStore', {
+    state: () => ({
+        data: [],
+    }),
+
+    actions: {},
+})
