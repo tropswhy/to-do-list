0e7acc939d081b46d9ca4e26e83916a380dfab9f
tropswhy <solodkovnikita08@gmail.com>
2024-01-19 23:43:34 +0300

    add Input.vue

---
+++ src/ui-kit/Input.vue
@@ -0,0 +1,26 @@
+<script setup>
+const props = defineProps(['styles', 'placeholder', 'type'])
+</script>
+
+<template>
+        <input
+            :class="'input ' + styles"
+            :placeholder='placeholder'
+            :type='type'
+            />
+</template>
+
+<style lang='scss' scoped>
+.input {
+    border-radius: $border-small;
+    box-shadow: 0 0 7px $secondary-color;
+    height: 50px;
+    width: 500px;
+    padding: 0 20px;
+    transition-duration: 1500ms;
+    &:focus {
+        outline: 0;
+        box-shadow: 0 0 14px $secondary-color;
+    }
+}
+</style>
