3f4caaf7dbf1da130d718079622f97c91cb75032
tropswhy <solodkovnikita08@gmail.com>
2024-01-22 16:40:21 +0300

    change addDo, add findByID and removeDo

--- src/stores/myStore.js
+++ src/stores/myStore.js
@@ -5,25 +5,51 @@ export const useMyStore = defineStore('myStore', {
         list: [
             {
                 id: 1,
-                title: "Обоссать кому-нибудь ебало"
+                title: 'Сделать to do list',
+                count: 0,
             },
             {
                 id: 2,
-                title: "Сделать тудудудудудудуд"
+                title: 'Подготовиться к экзамену по ТОЭ',
+                count: 0,
             },
             {
                 id: 3,
-                title: "дудудудудудуду"
+                title: 'Сделать курсач по ТАУ',
+                count: 0,
             },
         ],
     }),
 
     actions: {
-        addDo(title){
-            this.list = [...this.list, {
-                id: Date.now(),
-                title: title
-            }]
-        }
+        addDo(title) {
+            if (title.replaceAll(' ', '') !== '') {
+                this.list = [
+                    ...this.list,
+                    {
+                        id: Date.now(),
+                        title: title,
+                        count: 0,
+                    },
+                ]
+            } else {
+                alert('Вы оставили поле ввода пустым')
+            }
+        },
+
+        findById(id) {
+            let ind = -1
+            this.list.map((item, index) =>
+                item.id == id ? (ind = index) : (ind = ind)
+            )
+            if (ind == -1) return undefined
+            return this.list[ind]
+        },
+
+        removeDo(id) {
+            this.findById(id).count += 1
+            if (this.findById(id).count > 1)
+                this.list = this.list.filter((item) => item.id !== id)
+        },
     },
 })
