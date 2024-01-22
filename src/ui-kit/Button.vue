604c825a7e445ef426ebdb03b0f2645e81e9d471
tropswhy <solodkovnikita08@gmail.com>
2024-01-22 16:35:27 +0300

    add component Button

---
+++ src/ui-kit/Button.vue
@@ -0,0 +1,20 @@
+<template>
+    <button class="btn">
+        <slot></slot>
+    </button>
+</template>
+
+<style lang="scss" scoped>
+.btn {
+    width: 100px;
+    height: 50px;
+    padding: 0 10px;
+    background-color: $secondary-color;
+    border-radius: $border-medium;
+    transition-duration: 500ms;
+    &:hover {
+        background-color: $accent-color;
+        box-shadow: 0 0 10px $accent-color;
+    }
+}
+</style>
